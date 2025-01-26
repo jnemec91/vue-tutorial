c8d02e369a70c0d2b7e37708c0c154948cd1876f
Jaroslav Němec <j.nemecc9@gmail.com>
2025-01-26 16:25:34 +0100

    outputing data

--- 1/app.js
+++ 1/app.js
@@ -1,6 +1,13 @@
 
 const app = Vue.createApp({
     // template: '<h2>Im the template</h2>'
+    data() {
+        return {
+            title: 'The Lord of the Rings',
+            author: 'J.R.R. Tolkien',
+            nationality: 'UK'
+        }
+    }
 })
 
 app.mount('#app')
