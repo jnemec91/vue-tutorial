bf5c1e3eb2a7314fc9e6c2314a419dfecc486254
Jaroslav Němec <j.nemecc9@gmail.com>
2025-01-26 18:34:28 +0100

    computed properties

--- 1/app.js
+++ 1/app.js
@@ -63,6 +63,11 @@ const app = Vue.createApp({
         toggleFav(book) {
             book.isFav = !book.isFav
         }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
+        }
     }
 })
 
