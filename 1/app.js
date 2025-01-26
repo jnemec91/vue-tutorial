dccb253da3f2b50128112d76465a203c77d0c650
Jaroslav Němec <j.nemecc9@gmail.com>
2025-01-26 17:32:12 +0100

    outputing arrays

--- 1/app.js
+++ 1/app.js
@@ -10,6 +10,11 @@ const app = Vue.createApp({
             price: 1,
             x: 0,
             y: 0,
+            books: [
+                {title:'Metro 2033', author:'Dmitrij Gluchovskij'},
+                {title:'Witcher', author:'Andrzej Sapkowski'},
+                {title:'The Martian', author:'Andy Weir'},
+            ]
         }
     },
     methods: {
@@ -20,7 +25,19 @@ const app = Vue.createApp({
             this.showBooks = !this.showBooks
         },
         handleEvents(e) {
+            console.log(e)
             console.log(e.type)
+            switch (e.type){
+                case 'mouseover':
+                    e.srcElement.style.background = 'red'
+                    break                
+                case 'mouseleave':
+                    e.srcElement.style.background = 'orangered'
+                    break
+                case 'dblclick':
+                    e.srcElement.style.background = 'green'
+                    break
+            }
         },
         handleMouseMove(e){
             this.x = e.offsetX
