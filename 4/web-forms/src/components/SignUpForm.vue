7d8f59eda51ca1f368df771e97813ed13941b878
Jaroslav Němec <j.nemecc9@gmail.com>
2025-01-28 20:48:37 +0100

    Checkboxes, Grouping checkboxes

--- 4/web-forms/src/components/SignUpForm.vue
+++ 4/web-forms/src/components/SignUpForm.vue
@@ -8,11 +8,37 @@
         <label>Role:</label>
         <select v-model="role">
             <option value="developer">Web developer</option>
-            <option value="deigner">Web designer</option>
+            <option value="designer">Web designer</option>
         </select>
-                
+
+        <h2>Checkboxes</h2>
+        <div class="terms">
+          <input type="checkbox" v-model="terms" required>
+          <label>Accept terms and conditions</label>
+        </div>
+        
+        <h2>Grouping v-models</h2>
+        <div>
+          <input type="checkbox" value="Jarda" v-model="names">
+          <label>Jarda</label>
+        </div>
+
+        <div>
+          <input type="checkbox" value="Pepa" v-model="names">
+          <label>Pepa</label>
+        </div>        
+
+        <div>
+          <input type="checkbox" value="Honza" v-model="names">
+          <label>Honza</label>
+        </div>  
+
     </form>
-    <p>{{email }} - {{ password }} - {{role}}</p>
+    <p>Email: {{email }}</p>
+    <p>Password: {{ password }}</p>
+    <p>Role: {{role}}</p>
+    <p>Accepted terms: {{ terms }}</p>
+    <p>Selected names: {{ names }}</p>
 </template>
 
 <script>
@@ -22,7 +48,9 @@ export default {
     return {
       email: '',
       password: '',
-      role: 'developer'
+      role: 'developer',
+      terms: false,
+      names: []
     }
   }    
 }
