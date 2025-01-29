4708eca982f8e78691bf77fa9d87e41334ee8a0b
Jaroslav Němec <j.nemecc9@gmail.com>
2025-01-29 20:55:37 +0100

    computed properties with composition api

--- 6/dojo-blog/src/views/HomeView.vue
+++ 6/dojo-blog/src/views/HomeView.vue
@@ -1,46 +1,33 @@
 <template>
   <div class="home">
     <h2>Home</h2>
-    <h3>Refs</h3>
-    <p>{{ person.name }} - {{ person.age }}</p>
-    <button @click="updatePerson">Update person</button>
-    <h3>Reactive</h3>
-    <p>{{ another.name }} - {{ another.age }}</p>
-    <button @click="updateAnotherPerson">Update another person</button>
+    <h3>Computed</h3>
+    <input type="text" v-model="search">
+    <p>search term: {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      <p>{{ name }}</p>
+    </div>
+
   </div>
 
 </template>
 
 <script>
 // @ is an alias to /src
-import { ref, reactive } from "vue";
+import { ref, computed } from "vue";
 
 export default {
   name: 'HomeView',
   setup() {
-    // safer to use, still remain reactive after getting exposed to external funcs
-    const person = ref({
-      name: 'Jarda',
-      age: 33
+    const names = ref(['jarda', 'honza', 'pepa'])
+    const search = ref('')
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
     })
-
-    // reactive isnt reactive with primitive values (lol wtf), but at least you dont need to \
-    // type .value when accessing the data.
-    const another = reactive({
-      name: 'Honza',
-      age: 33
-    })
-
-    const updatePerson = () => {
-      person.value.age++
-    }
-
-    const updateAnotherPerson = () => {
-      another.age++
-    }    
 
     return {
-      person, updatePerson, another, updateAnotherPerson
+      names, search, matchingNames
     }
   },
 }
