08707e2ed30b364b2143aceb65939638f5cd986e
Jaroslav Němec <j.nemecc9@gmail.com>
2025-01-29 20:35:14 +0100

    refs with composition api

--- 6/dojo-blog/src/views/HomeView.vue
+++ 6/dojo-blog/src/views/HomeView.vue
@@ -1,27 +1,37 @@
 <template>
   <div class="home">
     <h2 @click="handleClick">Home</h2>
-    My name {{ name }} and my age is {{ age }}
+    <p ref="p">My name {{ name }} and my age is {{ age }}</p>
   </div>
+  <input type="text" v-model="name">
+  <button @click="age++">Age++</button>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { ref } from "vue";
 
 export default {
   name: 'HomeView',
   setup() {
+    const p = ref(null)
+    console.log(p, p.value)
+
     console.log('setup')
-    let name = 'jarda'
-    let age = 33
+    // ref makes variable reactive (vue reacts to their change and updates dom)
+    let name = ref('jarda')
+    let age = ref(33)
+
+    // when assigning to the refs, use .value
+    age.value += 1
 
     const handleClick = () => {
-      console.log('Youve killed me!')
+      console.log(p, p.value)
+      p.value.textContent = 'Button clicked'
     }
 
     return {
-      name, age, handleClick
+      name, age, handleClick, p
     }
   },
 }
