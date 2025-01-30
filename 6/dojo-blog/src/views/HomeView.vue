490eac0fd5d40a71523d71bbfb04d6b5aeed4f52
Jaroslav Nemec <j.nemecc9@gmail.com>
2025-01-30 08:41:58 +0100

    fetching data with async / await

--- 6/dojo-blog/src/views/HomeView.vue
+++ 6/dojo-blog/src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="home">
     <h2>Home</h2>
-    <PostList v-if="showPosts" :posts="posts"></PostList>
-    <button @click="showPosts = !showPosts">toggle posts</button>
-    <button @click="posts.pop()">Delete a post</button>
+    <div v-if="!error">
+      <div v-if="posts.length">
+        <PostList v-if="showPosts" :posts="posts"></PostList>
+        <button @click="showPosts = !showPosts">toggle posts</button>
+        <button @click="posts.pop()">Delete a post</button>
+      </div>
+      <div v-else>
+        <h3>Loading...</h3>
+      </div>
+    </div>
+    <div v-else>
+        {{ error }}
+    </div>
+
   </div>
 
 </template>
@@ -19,13 +30,33 @@ export default {
   setup() {
     const posts = ref([])
     const showPosts = ref(true)
+    const error = ref(null)
 
-    fetch('http://localhost:3000/posts')
-    .then(response => response.json())
-    .then(data => posts.value = data)
-    .catch(err => alert(err))
+    // fetch('http://localhost:3000/posts')
+    // .then(response => response.json())
+    // .then(data => posts.value = data)
+    // .catch(err => alert(err))
+
+    // loading with async / await
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw Error('No data available')
+        }
+        posts.value = await data.json() // this is also asynchronous
+
+        return data
+      }
+      catch (err){
+        error.value = err.message
+        alert(error.value)
+      }
+    }
+    posts.value = load()
+
     return {
-      posts, showPosts
+      posts, showPosts, error
     }
   },
 }
